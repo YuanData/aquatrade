e6e0c86be703d310df37b68aa244244fa47bf89c
Alan <kcykey@gmail.com>
2023-07-03 22:48:41 +0800

    Add load config for environment variables from file

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,20 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/YuanData/aquatrade/util"
 	_ "github.com/lib/pq"
 )
 
 var testQueries *Queries
 var testDB *sql.DB
 
-const (
-	DBDriver = "postgres"
-	DBSource = "postgresql://root:Aquamarine@localhost:5432/aquatrade?sslmode=disable"
-)
-
 func TestMain(m *testing.M) {
-	var err error
-	testDB, err = sql.Open(DBDriver, DBSource)
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config")
+	}
+
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}

1095c01b834fe47c13f3689ba3c9aa387fc2b175
Alan <kcykey@gmail.com>
2023-07-05 16:14:49 +0800

    Refactoring by t.Run add more tc

--- api/trader_test.go
+++ api/trader_test.go
@@ -1,7 +1,10 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -17,25 +20,48 @@ import (
 func TestGetTraderAPI(t *testing.T) {
 	trader := randomTrader()
 
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
+	testCases := []struct {
+		name          string
+		traderID      int64
+		buildStubs    func(store *mockdb.MockStore)
+		checkResponse func(t *testing.T, recoder *httptest.ResponseRecorder)
+	}{
+		{
+			name:     "OK",
+			traderID: trader.ID,
+			buildStubs: func(store *mockdb.MockStore) {
+				store.EXPECT().
+					GetTrader(gomock.Any(), gomock.Eq(trader.ID)).
+					Times(1).
+					Return(trader, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchTrader(t, recorder.Body, trader)
+			},
+		},
+	}
 
-	store := mockdb.NewMockStore(ctrl)
-	store.EXPECT().
-		GetTrader(gomock.Any(), gomock.Eq(trader.ID)).
-		Times(1).
-		Return(trader, nil)
+	for i := range testCases {
+		tc := testCases[i]
+		t.Run(tc.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-	server := NewServer(store)
-	recorder := httptest.NewRecorder()
+			store := mockdb.NewMockStore(ctrl)
+			tc.buildStubs(store)
 
-	url := fmt.Sprintf("/traders/%d", trader.ID)
-	request, err := http.NewRequest(http.MethodGet, url, nil)
-	require.NoError(t, err)
+			server := NewServer(store)
+			recorder := httptest.NewRecorder()
 
-	server.router.ServeHTTP(recorder, request)
+			url := fmt.Sprintf("/traders/%d", trader.ID)
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
 
-	require.Equal(t, http.StatusOK, recorder.Code)
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
+	}
 }
 
 func randomTrader() db.Trader {
@@ -45,4 +71,13 @@ func randomTrader() db.Trader {
 		Balance:  util.RandomMoney(),
 		Currency: util.RandomCurrency(),
 	}
+}
+func requireBodyMatchTrader(t *testing.T, body *bytes.Buffer, trader db.Trader) {
+	data, err := ioutil.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotTrader db.Trader
+	err = json.Unmarshal(data, &gotTrader)
+	require.NoError(t, err)
+	require.Equal(t, trader, gotTrader)
 }
